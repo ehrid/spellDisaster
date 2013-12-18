95c17c57ac0bfc5ff5115a7855f82fe61d4a484d
ehrid <zygota.v2@gmail.com>
2013-12-18 14:31:36 +0100

    added font and edit text to diary layout

--- src/com/example/spelldisaster/interfaces/InterfaceDiary.java
+++ src/com/example/spelldisaster/interfaces/InterfaceDiary.java
@@ -1,8 +1,10 @@
 package com.example.spelldisaster.interfaces;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.EditText;
 
 import com.example.spelldisaster.R;
 
@@ -11,13 +13,18 @@ import com.example.spelldisaster.R;
  */
 public class InterfaceDiary extends BaseInterfaceNoFrameActivity {
 
+    private EditText _leftPage;
+
+    private EditText _rightPage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         View inflated = initializeDialogBody();
+        initializePages(inflated);
+        setPagesFont();
 
-        // fill
     }
 
     private View initializeDialogBody() {
@@ -25,6 +32,17 @@ public class InterfaceDiary extends BaseInterfaceNoFrameActivity {
         stub.setLayoutResource(R.layout.dialog_diary);
         View inflated = stub.inflate();
         return inflated;
+    }
+
+    private void initializePages(View inflated) {
+        _leftPage = (EditText) inflated.findViewById(R.id.diary_left);
+        _rightPage = (EditText) inflated.findViewById(R.id.diary_right);
+    }
+
+    private void setPagesFont() {
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/ASafePlacetoFall.ttf");
+        _leftPage.setTypeface(font);
+        _rightPage.setTypeface(font);
     }
 
     @Override
