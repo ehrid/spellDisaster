fa494ad3639494a6be0da3b7151ac8070fe6320e
ehrid <zygota.v2@gmail.com>
2013-12-19 12:25:27 +0100

    added draft of inventory

--- src/com/example/spelldisaster/interfaces/InterfaceInventory.java
+++ src/com/example/spelldisaster/interfaces/InterfaceInventory.java
@@ -1,28 +1,51 @@
 package com.example.spelldisaster.interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewStub;
-import android.widget.RelativeLayout;
+import android.widget.GridView;
 
 import com.example.spelldisaster.R;
+import com.example.spelldisaster.inventory.InventoryGridViewAdapter;
+import com.example.spelldisaster.inventory.InventoryItem;
 
 /**
  * @author horodysk
  */
 public class InterfaceInventory extends BaseInterfaceActivity {
+
+    List<InventoryItem> gridArray = new ArrayList<InventoryItem>();
+
+    InventoryGridViewAdapter customGridAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setDialogBackground();
-
         View inflated = initializeDialogBody();
-    }
 
-    private void setDialogBackground() {
-        RelativeLayout background = (RelativeLayout) findViewById(R.id.dialog_frame);
-        background.setBackgroundResource(R.drawable.style_dialog_paper_background);
+        View inventory = inflated.findViewById(R.id.inventory);
+        GridView container = (GridView) inventory.findViewById(R.id.inventory_countainer);
+
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+        gridArray.add(new InventoryItem());
+
+        customGridAdapter = new InventoryGridViewAdapter(this, R.layout.inventory_item, gridArray);
+        container.setAdapter(customGridAdapter);
+
     }
 
     private View initializeDialogBody() {
---
+++ src/com/example/spelldisaster/inventory/InventoryItem.java
@@ -0,0 +1,17 @@
+package com.example.spelldisaster.inventory;
+
+import com.example.spelldisaster.R;
+
+/**
+ * @author horodysk
+ */
+public class InventoryItem {
+
+    // FIXME MOCK!
+
+    /***/
+    public int getImage() {
+        return R.drawable.ic_launcher;
+    }
+
+}
